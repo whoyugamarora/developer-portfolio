bb922b56e8b748c150b78dcfc14ca85240edb7ed
whoyugamarora <yugam.arora@student.ufv.ca>
2022-02-09 13:31:16 -0800

    Creating website from beginning using Bootstrap

--- script.js
+++
@@ -1,9 +0,0 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarlinks = document.getElementsByClassName('navlinks')[0]
-const resumebutton = document.getElementsByClassName('resume')[0]
-
-toggleButton.addEventListener('click', () => {
-    navbarlinks.classList.toggle('active')
-    resumebutton.classList.toggle('passive')
-})
-
