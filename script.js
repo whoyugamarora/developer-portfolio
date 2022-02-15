d02f62e7814dee1ab3553646b11d855289543b31
whoyugamarora <yugam.arora@student.ufv.ca>
2022-02-15 14:47:54 -0800

    Added script in html file instead of script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-const navlinks = document.getElementById('navlinks');
-
-hamburger.addEventListener('click', () => {
-    navlinks.classList.toggle('show');
-});
